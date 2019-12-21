81a79470550824ab9270fe8385dcbb6e36a1af09
dc <chendana95@gmail.com>
2019-12-21 05:14:32 -0500

    changed id so that for loop can increment up to 18th hour to retrieve from storage

--- script.js
+++ script.js
@@ -2,33 +2,6 @@ $(document).ready(function() {
     var currentTime = moment().local().format("dddd, MMMM Do YYYY, hh:mm a");
     $("#currentDay").text(currentTime);
 
-
-
-    for (var i = 9; i < 18; i++) {
-        var blockTime = i;
-        //   checkTime(blockTime);
-    }
-
-
-
-    // function checkTime(time) {
-    //     if (currentTime == time) {
-    //         // color is red
-    //         $("#" + [time]).attr("class", "col-md-10 description present");
-    //     } else if (currentTime >= time) {
-    //         // color is gray
-    //         $("#" + [time]).attr("class", "col-md-10 description past");
-    //     } else {
-    //         // color is green
-    //         $("#" + [time]).attr("class", "col-md-10 description future");
-    //     }
-    // }
-
-
-
-
-
-
     $(".saveBtn").on("click", function() {
         //console.log(this);
 
@@ -45,9 +18,8 @@ $(document).ready(function() {
         setColor($("." + i), i);
         var retrieve = localStorage.getItem("hour-" + i);
         if (retrieve !== null) {
-            console.log("HELLO");
             // $("#" + i).text(retrieve);
-            //$("." + i).append(JSON.parse(retrieve));
+            $("." + i).text(JSON.parse(retrieve));
         }
     }
 
